544da59af8dc53ddbfd60f4f0717c24c829736cf
Shorno <fb.shorno@gmail.com>
2024-06-13 18:32:34 +0600

    fancy counter project

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,4 +1,4 @@
-import { GrPowerReset } from "react-icons/gr";
+import {GrPowerReset} from "react-icons/gr";
 
 export default function Counter({counter, setCounter}) {
     const decrease = () => {
@@ -15,15 +15,23 @@ export default function Counter({counter, setCounter}) {
         <>
             <div className={"flex items-center justify-center h-full"}>
                 <div
-                    className={"text-3xl flex-col font-semibold text-[#385170] uppercase flex  w-1/4 h-1/2 bg-[#9fd3c7] text-center rounded-xl pt-10"}>
+                    className={"text-3xl flex-col font-semibold text-[#385170] uppercase flex  w-1/4 h-1/2 bg-[#9fd3c7] rounded-xl text-center pt-10"}>
                     <h1>Fancy <br/>Counter</h1>
                     <h1 className={"text-9xl pt-10 pb-10"}>{counter}</h1>
                     <button className={"flex justify-center hover:scale-125 transition duration-500 "}>
                         <GrPowerReset size={40} onClick={reset}/>
                     </button>
-                    <div className={"justify-around flex bg-[#ececec] rounded-b-xl h-1/4 mt-12"}>
-                        <button className={"text-8xl  hover:scale-75 transition duration-500 "} onClick={decrease}>-</button>
-                        <button className={"text-8xl hover:scale-75 transition duration-500s"} onClick={increase}>+</button>
+                    <div className={"flex items-center h-1/4 mt-12 gap-0.5 bg-[#9fd3c7]"}>
+                        <div className={"w-1/2 bg-amber-200 h-full hover:bg-amber-300 transition duration-500"}>
+                            <button className={"text-6xl hover:scale-75 transition duration-500 "}
+                                    onClick={decrease}>-
+                            </button>
+                        </div>
+                        <div className={"w-1/2 bg-amber-200 h-full hover:bg-amber-300 transition duration-500"}>
+                            <button className={"text-6xl hover:scale-75 transition duration-500s"}
+                                    onClick={increase}>+
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
